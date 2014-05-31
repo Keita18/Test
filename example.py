003151f91db2a4af9fe7f20beb3b015cdcc9d7ec
Keita Tanji <ijnat18@gmail.com>
2014-05-31 16:39:18 +0900

    Add testfile

---
+++ example.py
@@ -0,0 +1,7 @@
+import sublime, sublime_plugin
+import os
+
+class ExampleCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		self.view.insert(edit, 0, "Hello, test!")
+		os.system("sleep")
